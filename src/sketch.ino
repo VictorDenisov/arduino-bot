e4ba6ae99c312be3cf6d51c21babb36cd91ab2ea
Victor Denisov <denisovenator@gmail.com>
2017-01-04 09:15:49 -0800

    Initial import

---
+++ src/sketch.ino
@@ -0,0 +1,105 @@
+#define trig_pin 7
+#define echo_pin 8
+
+void setup()
+{
+	pinMode(A1, OUTPUT);
+	pinMode(A2, OUTPUT);
+	pinMode(A3, OUTPUT);
+	pinMode(A4, OUTPUT);
+	pinMode(9, OUTPUT);
+	pinMode(10, OUTPUT);
+
+	pinMode(trig_pin, OUTPUT);
+	pinMode(echo_pin, INPUT);
+
+}
+
+void go_forward() {
+	digitalWrite(A1, LOW);
+	digitalWrite(A2, HIGH);
+	digitalWrite(A3, LOW);
+	digitalWrite(A4, HIGH);
+	/*
+	analogWrite(9, 230);
+	analogWrite(10, 217);
+	*/
+	analogWrite(9, 120);
+	analogWrite(10, 120);
+}
+
+void go_backward() {
+	digitalWrite(A1, HIGH);
+	digitalWrite(A2, LOW);
+	digitalWrite(A3, HIGH);
+	digitalWrite(A4, LOW);
+	/*
+	analogWrite(9, 230);
+	analogWrite(10, 217);
+	*/
+	analogWrite(9, 120);
+	analogWrite(10, 120);
+}
+
+void stop() {
+	digitalWrite(A1, LOW);
+	digitalWrite(A2, LOW);
+	digitalWrite(A3, LOW);
+	digitalWrite(A4, LOW);
+	analogWrite(9, 0);
+	analogWrite(10, 0);
+}
+
+unsigned long get_distance() {
+	digitalWrite(trig_pin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trig_pin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(trig_pin, LOW);
+
+	unsigned long echo = pulseIn(echo_pin, HIGH);
+	return (1.0 * echo / 58.138);
+}
+
+void turnLeft() {
+	digitalWrite(A1, HIGH);
+	digitalWrite(A2, LOW);
+	digitalWrite(A3, LOW);
+	digitalWrite(A4, HIGH);
+	analogWrite(9, 120);
+	analogWrite(10, 120);
+}
+
+void turnRight() {
+	digitalWrite(A1, LOW);
+	digitalWrite(A2, HIGH);
+	digitalWrite(A3, HIGH);
+	digitalWrite(A4, LOW);
+	analogWrite(9, 120);
+	analogWrite(10, 120);
+}
+
+void loop()
+{
+	unsigned long distance = get_distance();
+	if (distance < 5) {
+		stop();
+		turnLeft();
+		delay(500);
+		stop();
+		unsigned long leftDistance = get_distance();
+		turnRight();
+		delay(1000);
+		stop();
+		unsigned long rightDistance = get_distance();
+		if (rightDistance < leftDistance) {
+			turnLeft();
+			delay(1000);
+			stop();
+		}
+	} else {
+		go_forward();
+	}
+}
+
+// vim: ai cin
