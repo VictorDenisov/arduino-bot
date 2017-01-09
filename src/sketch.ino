6471d41aa8bf88d063839b4fa0e92157406ca686
Victor Denisov <denisovenator@gmail.com>
2017-01-08 19:19:50 -0800

    Add support for compass

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,3 +1,110 @@
+#include <Wire.h>
+
+#define _X 0
+#define _Y 0
+#define _Z 0
+
+byte read(int reg)
+{
+	Wire.beginTransmission(0x68);
+	Wire.write(reg);
+	Wire.endTransmission(false);
+	Wire.requestFrom(0x68, 1, false);
+	byte val = Wire.read();
+	Wire.endTransmission(true);
+	return val;
+}
+
+void write(int reg, int data)
+{
+	Wire.beginTransmission(0x68);
+	Wire.write(reg);
+	Wire.write(data);
+	Wire.endTransmission(true);
+}
+
+byte readMag(int reg)
+{
+	Wire.beginTransmission(0x0C);
+	Wire.write(reg);
+	Wire.endTransmission(false);
+	Wire.requestFrom(0x0C, 1, false);
+	byte val = Wire.read();
+	Wire.endTransmission(true);
+	return val;
+}
+
+void writeMag(int reg, int data)
+{
+	Wire.beginTransmission(0x0C);
+	Wire.write(reg);
+	Wire.write(data);
+	Wire.endTransmission(true);
+}
+
+double scalarMult(double a[3], double b[3])
+{
+	double res = 0;
+	for (int i = 0; i < 3; ++i) {
+		res += a[i] * b[i];
+	}
+	return res;
+}
+
+void vectorMult(double a[3], double b[3], double res[3])
+{
+	for (int i = 0; i < 3; ++i) {
+		res[i] = a[(i + 1) % 3] * b[(i + 2) % 3] - a[(i + 2) % 3] * b[(i + 1) % 3];
+	}
+}
+
+double tot[3]; // x, y, z
+
+bool curAvailable;
+bool sAvailable;
+double cur[3]; // x, y, z
+double now[3]; // x, y, z
+
+int magMeasurementNumber;
+
+bool calibrated;
+
+double b1[3]; // x, y, z
+double b2[3]; // x, y, z
+double bb[3];
+
+void doMeasurement(int measureCount)
+{
+	int xh = readMag(0x04);
+	int xl = readMag(0x03);
+	int yh = readMag(0x06);
+	int yl = readMag(0x05);
+	int zh = readMag(0x08);
+	int zl = readMag(0x07);
+
+	readMag(0x09);
+
+	double x = (xh << 8) | (xl & 0xff);
+	double y = (yh << 8) | (yl & 0xff);
+	double z = (zh << 8) | (zl & 0xff);
+
+	if (magMeasurementNumber < measureCount) {
+		tot[_X] += x;
+		tot[_Y] += y;
+		tot[_Z] += z;
+		++magMeasurementNumber;
+	} else {
+		for (int i = 0; i < 3; ++i) {
+			now[i] = tot[i] / measureCount;
+		}
+		tot[_X] = x;
+		tot[_Y] = y;
+		tot[_Z] = z;
+		magMeasurementNumber = 1;
+		sAvailable = true;
+	}
+}
+
 #define trig_pin 7
 #define echo_pin 8
 
@@ -13,11 +120,24 @@ void setup()
 	pinMode(trig_pin, OUTPUT);
 	pinMode(echo_pin, INPUT);
 
+	Wire.begin();
+	write(0x6B, 0);
+	write(0x6A, 0);
+	write(0x37, 0x02);
+	writeMag(0x0A, 0x12);
+	for (int i = 0; i < 3; ++i) {
+		tot[i] = 0;
+	}
+	magMeasurementNumber = 0;
+	curAvailable = false;
+	sAvailable = false;
+	calibrated = false;
 }
 
 const int speed = 80;
 
-void go_forward() {
+void go_forward()
+{
 	digitalWrite(A1, LOW);
 	digitalWrite(A2, HIGH);
 	digitalWrite(A3, LOW);
@@ -26,11 +146,28 @@ void go_forward() {
 	analogWrite(9, 230);
 	analogWrite(10, 217);
 	*/
-	analogWrite(9, speed);
-	analogWrite(10, speed);
+	if (curAvailable) {
+		double dir[3];
+		vectorMult(cur, now, dir);
+		double sm = scalarMult(dir, bb);
+		if (sm > 0) {
+			analogWrite(9, speed);
+			analogWrite(10, speed - 5);
+		} else if (sm < 0) {
+			analogWrite(9, speed - 5);
+			analogWrite(10, speed);
+		} else {
+			analogWrite(9, speed);
+			analogWrite(10, speed);
+		}
+	} else {
+		analogWrite(9, speed);
+		analogWrite(10, speed);
+	}
 }
 
-void go_backward() {
+void go_backward()
+{
 	digitalWrite(A1, HIGH);
 	digitalWrite(A2, LOW);
 	digitalWrite(A3, HIGH);
@@ -43,7 +180,8 @@ void go_backward() {
 	analogWrite(10, speed);
 }
 
-void stop() {
+void stop()
+{
 	digitalWrite(A1, LOW);
 	digitalWrite(A2, LOW);
 	digitalWrite(A3, LOW);
@@ -52,7 +190,8 @@ void stop() {
 	analogWrite(10, 0);
 }
 
-unsigned long get_distance() {
+unsigned long get_distance()
+{
 	digitalWrite(trig_pin, LOW);
 	delayMicroseconds(2);
 	digitalWrite(trig_pin, HIGH);
@@ -63,7 +202,8 @@ unsigned long get_distance() {
 	return (1.0 * echo / 58.138);
 }
 
-void turnLeft() {
+void turnLeft()
+{
 	digitalWrite(A1, HIGH);
 	digitalWrite(A2, LOW);
 	digitalWrite(A3, LOW);
@@ -72,7 +212,8 @@ void turnLeft() {
 	analogWrite(10, speed);
 }
 
-void turnRight() {
+void turnRight()
+{
 	digitalWrite(A1, LOW);
 	digitalWrite(A2, HIGH);
 	digitalWrite(A3, HIGH);
@@ -81,10 +222,48 @@ void turnRight() {
 	analogWrite(10, speed);
 }
 
+void doCalibrate()
+{
+	for (int i = 0; i < 502; ++i) {
+		doMeasurement(500);
+		delay(1);
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		b1[i] = now[i];
+	}
+	turnLeft();
+	delay(300);
+	stop();
+
+	for (int i = 0; i < 502; ++i) {
+		doMeasurement(500);
+		delay(1);
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		b2[i] = now[i];
+	}
+
+	vectorMult(b1, b2, bb);
+}
+
 void loop()
 {
+	if (!calibrated) {
+		doCalibrate();
+	}
+	doMeasurement(500);
+
+	if (sAvailable) {
+		for (int i = 0; i < 3; ++i) {
+			cur[i] = now[i];
+		}
+		curAvailable = true;
+	}
+
 	unsigned long distance = get_distance();
-	if (distance < 5) {
+	if (distance < 8) {
 		stop();
 		turnLeft();
 		delay(500);
